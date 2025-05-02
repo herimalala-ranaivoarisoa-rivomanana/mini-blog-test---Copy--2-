69e1b85cb8687f71278132ef90a523f9675f4258
Herimalala <h.rivomanana@bdo.mu>
2025-05-02 20:14:20 +0300

    fix:api

--- src/lib/articles.ts
+++ src/lib/articles.ts
@@ -4,10 +4,11 @@ import axios from 'axios';
 const isServer = typeof window === 'undefined';
 
 // 🔁 Base URL selon contexte
-const baseUrl = isServer
+/* const  =baseUrl isServer
   ? process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'
   : '';
-
+ */
+  const baseUrl = ''
 console.log('🔗 Base URL utilisée pour Axios:', baseUrl);
 
 export const getAllArticles = async (): Promise<Article[]> => {
